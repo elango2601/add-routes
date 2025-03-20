a5715c4293a4d494756f68ed33c1aefa0277079c
Elango <elangopugal26@mail.com>
2025-03-20 11:35:26 +0530

    This is flask

--- hello_flask.py
+++ hello_flask.py
@@ -1,14 +1,10 @@
 from flask import Flask
+
 app=Flask(__name__)
-@app.route("/hello")
+
+@app.route("/")
 def index():
-    return "Flask Working fine"
-@app.route("/laptop")
-def india():
-    return "abcddef"
+    return "Flask working fine"
+  
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
-
-
-    
-    
+    app.run()
